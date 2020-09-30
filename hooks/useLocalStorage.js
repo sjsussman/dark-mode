abbd60af2fa3cf36308f793aae72b170d5d29701
Steven Sussman <sjsussman@gmail.com>
2020-09-30 15:02:20 -0400

    created useLocalStorage hook

--- hooks/useLocalStorage.js
+++ hooks/useLocalStorage.js
@@ -1,5 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
-const UseLocalStorage = () => {};
+const UseLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    const item = window.localStorage.getItem(key);
+    return item ? JSON.parse(item) : initialValue;
+  });
+
+  const setValue = (value) => {
+    setStoredValue(value);
+    window.localStorage.setItem(key, JSON.stringify(value));
+  };
+
+  return [storedValue];
+};
 
 export default UseLocalStorage;
